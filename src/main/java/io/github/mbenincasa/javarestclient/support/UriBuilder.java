8bb4e8f979203121fa35c4274bdc9212d1269353
MBenincasa <mirkobenincasa44@gmail.com>
2024-05-02 21:36:11 +0200

    Using UriBuilder it is possible to add parameters to queries

--- src/main/java/io/github/mbenincasa/javarestclient/support/UriBuilder.java
+++ src/main/java/io/github/mbenincasa/javarestclient/support/UriBuilder.java
@@ -1,10 +1,19 @@
 package io.github.mbenincasa.javarestclient.support;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class UriBuilder {
 
     private URI uri;
+    private Map<String, List<Object>> queryParams;
+
+    private UriBuilder() {
+        this.queryParams = new HashMap<>();
+    }
 
     public static UriBuilder create() {
         return new UriBuilder();
@@ -15,7 +24,37 @@ public class UriBuilder {
         return this;
     }
 
+    public UriBuilder uri(String uri) {
+        this.uri = URI.create(uri);
+        return this;
+    }
+
+    public UriBuilder queryParam(String name, Object... values) {
+        if (!queryParams.containsKey(name)) {
+            queryParams.put(name, new LinkedList<>());
+        }
+        for (Object value : values) {
+            queryParams.get(name).add(value);
+        }
+        return this;
+    }
+
     public RestRequestUri build() {
-        return new DefaultRestRequestUri(this.uri);
+        StringBuilder uriBuilder = new StringBuilder(this.uri.toString());
+
+        if (!queryParams.isEmpty()) {
+            uriBuilder.append("?");
+            queryParams.forEach((name, values) -> {
+                for (Object value : values) {
+                    uriBuilder.append(name)
+                            .append("=")
+                            .append(value.toString())
+                            .append("&");
+                }
+            });
+            uriBuilder.deleteCharAt(uriBuilder.length() - 1);
+        }
+
+        return new DefaultRestRequestUri(URI.create(uriBuilder.toString()));
     }
 }
